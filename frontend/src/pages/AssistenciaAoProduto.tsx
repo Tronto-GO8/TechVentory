e7b1132e4bf344f9a6c89c59bf76a80c49aa246c
Gustavo Jeziel <gustavofogaca85@gmail.com>
2025-11-19 17:57:36 -0300

    adicionando pequena alteração

--- frontend/src/pages/AssistenciaAoProduto.tsx
+++ frontend/src/pages/AssistenciaAoProduto.tsx
@@ -13,18 +13,18 @@ export default function Assistencia() {
   return (
     // Grid principal — o header global já existe fora daqui,
     // então esta parte ocupa apenas o restante da tela
-    <div className="grid grid-rows-[auto_1fr] max-h-[100vh]">
+    <div className="grid grid-rows-[auto_1fr] h-[100vh] dark:bg-gray-900 dark:border-white">
       {/* Conteúdo principal dividido entre FAQ e Chat */}
       <div
         className="grid grid-cols-[minmax(0,10%)_minmax(0,90%)] 
                       lg:grid-cols-[minmax(0,30%)_minmax(0,70%)] 
-                      overflow-hidden"
+                      overflow-hidden dark:bg-gray-900 dark:border-white"
       >
-        <div className="w-full h-full overflow-hidden">
+        <div className="w-full h-full overflow-hidden dark:bg-gray-900 dark:border-white">
           <BarraDeAssistencia onQuickReply={mensagemAoClicar} />
         </div>
 
-        <div className="w-full h-full overflow-hidden">
+        <div className="w-full h-full overflow-hidden dark:bg-gray-900 dark:border-white">
           <ChatIa preMessages={preMessages} setPreMessages={setPreMessages} />
         </div>
       </div>
